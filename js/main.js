a1bde4fa2bbcda379d254f98c25b5ebd83349f51
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-13 15:34:55 +0200

    modifiche finali

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+// RACCOLTA NOME
 var nome = prompt("Qual'e' il tuo nome?");
+// RACCOLTA COGNOME
 var cognome = prompt("Qual'e' il tuo cognome?");
+// RACCOLTA COLORE
 var colore = prompt("Qual'e' il tuo colore preferito?");
-
+// OUTPUT SCHERMO
 document.getElementById('password').innerHTML = nome+cognome+colore+19;
+// OUTPUT CONSOLE
+console.log(nome+cognome+colore+19);
