23ae6349aa8ba43edcccd96d0a06f03b862d48f8
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-13 15:23:28 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var nome = prompt("Qual'e' il tuo nome?");
+var cognome = prompt("Qual'e' il tuo cognome?");
+var colore = prompt("Qual'e' il tuo colore preferito?");
+
+document.getElementById('password').innerHTML = nome+cognome+colore+19;
